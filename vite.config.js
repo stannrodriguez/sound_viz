7f18fd9a79ca70bf33bbd5e3763a7e07f711f41b
Stephanie Rodriguez <stephsiriban@gmail.com>
2024-11-12 18:00:03 -0500

    update config to support github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/sound_viz/",
   plugins: [react()],
   resolve: {
     alias: {
